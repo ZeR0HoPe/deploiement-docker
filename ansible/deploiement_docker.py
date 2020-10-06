021c225a794901a7d5d2b4e6de5aead5743260f4
Matthieu.D <zerohope069@outlook.fr>
2020-10-06 15:12:29 +0200

    Update

--- ansible/deploiement_docker.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system('docker ps -a')
-
-
-ansible-playbook -i hosts playbook.yml --extra-vars '{"count":"4","container_name":"docker","container_image":"ubuntu_ssh"}'
--- deploiement_docker.py
+++ deploiement_docker.py
@@ -1,2 +1,5 @@
 import os
 os.system('docker ps -a')
+
+
+ansible-playbook -i hosts playbook.yml --extra-vars '{"count":"4","container_name":"docker","container_image":"ubuntu_ssh"}'
