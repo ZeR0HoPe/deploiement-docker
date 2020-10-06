91388a5e27304229b181513c10cc6921f94040e8
Matthieu.D <zerohope069@outlook.fr>
2020-10-06 16:23:13 +0200

    Update file

--- deploiement_docker.py
+++ deploiement_docker.py
@@ -1,5 +1,34 @@
-import os
-os.system('docker ps -a')
+import os, argparse
+
+"""
+Deploiement labs docker
+Author :    Matthieu DEBRAY
+Dependancies :
+    - os, argparse
+    - docker in OS
+Release :   V0.1  10/2020
+"""
 
 
-ansible-playbook -i hosts playbook.yml --extra-vars '{"count":"4","container_name":"docker","container_image":"ubuntu_ssh"}'
+
+def deploy(args):
+
+    dict={}
+    dict["count"] = args.count
+    dict["container_name"] = args.name
+    dict["container_image"] = args.docker
+    var = """
+        ansible-playbook -i ./ansible/hosts ./ansible/playbook.yml --extra-vars '{}'
+    """.format(dict)
+    os.system(var)
+
+
+## Main call
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Deploiement docker')
+    parser.add_argument('-c', '--count', help='Docker', type=str, required=True)
+    parser.add_argument('-n', '--name', help='Docker', type=str, required=True)
+    parser.add_argument('-d', '--docker', help='Image docker', type=str, required=True)
+    args = parser.parse_args()
+
+    deploy(args)
