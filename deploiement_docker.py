53ebddd4077fdacd9daf4cab575795643e7daaa7
Matthieu.D <zerohope069@outlook.fr>
2020-10-07 16:04:32 +0200

    Update file

--- deploiement_docker.py
+++ deploiement_docker.py
@@ -1,11 +1,13 @@
 import os, argparse
+import time
+
 
 """
 Deploiement labs docker
 Author :    Matthieu DEBRAY
 Dependancies :
     - os, argparse
-    - docker in OS
+    - docker and ansible in OS
 Release :   V0.1  10/2020
 """
 
@@ -17,10 +19,23 @@ def deploy(args):
     dict["count"] = args.count
     dict["container_name"] = args.name
     dict["container_image"] = args.docker
-    var = """
+    ans = """
         ansible-playbook -i ./ansible/hosts ./ansible/playbook.yml --extra-vars '{}'
     """.format(dict)
-    os.system(var)
+    os.system(ans)
+    time.sleep(2)
+
+
+    for i in range(1,int(args.count)+1,1):
+
+        var1 = "docker inspect -f '=> {{.Name}} - {{.NetworkSettings.IPAddress }}' "
+        var2 = (args.name)+str(i)
+        var3 = "docker inspect -f '{{.NetworkSettings.IPAddress }}' "
+        # Clean hots ip in ssh
+        # var4 = os.popen(var3+var2).read()
+        # os.system('ssh-keygen -R {}'.format(var4))
+        os.system(var1+var2)
+
 
 
 ## Main call
